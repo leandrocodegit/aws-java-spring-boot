b3a50f3365414907214f4d02d7c88e0762895ba8
leandrocodegit <lpoliveira.ti@gmail.com>
2026-06-09 20:34:58 -0300

    Hello

--- src/main/java/aws/demo/Controller.java
+++ src/main/java/aws/demo/Controller.java
@@ -1,6 +1,7 @@
 package aws.demo;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,7 @@ import java.util.Map;
 @RequestMapping("/helth")
 public class Controller {
 
+    @GetMapping
     public ResponseEntity<?> heath(){
         return ResponseEntity.ok(Map.of("status", "ok"));
     }
